e0f47d3c14230bc13cf46d79e2106de29653f502
Kha Gia Quach <khagia1001@gmail.com>
2021-05-13 17:05:10 -0500

    Add sinkhorn distance

--- src/trackformer/models/matcher.py
+++ src/trackformer/models/matcher.py
@@ -124,10 +124,168 @@ class HungarianMatcher(nn.Module):
                 for i, j in indices]
 
 
+class SinkhornDistance(torch.nn.Module):
+    r"""
+        Given two empirical measures each with :math:`P_1` locations
+        :math:`x\in\mathbb{R}^{D_1}` and :math:`P_2` locations :math:`y\in\mathbb{R}^{D_2}`,
+        outputs an approximation of the regularized OT cost for point clouds.
+        Args:
+        eps (float): regularization coefficient
+        max_iter (int): maximum number of Sinkhorn iterations
+        reduction (string, optional): Specifies the reduction to apply to the output:
+        'none' | 'mean' | 'sum'. 'none': no reduction will be applied,
+        'mean': the sum of the output will be divided by the number of
+        elements in the output, 'sum': the output will be summed. Default: 'none'
+        Shape:
+            - Input: :math:`(N, P_1, D_1)`, :math:`(N, P_2, D_2)`
+            - Output: :math:`(N)` or :math:`()`, depending on `reduction`
+    """
+
+    def __init__(self, eps=1e-3, max_iter=100, reduction='none', cost_class: float = 1, cost_bbox: float = 1, cost_giou: float = 1, focal_loss: bool = False, focal_alpha: float = 0.25):
+        super(SinkhornDistance, self).__init__()
+        self.eps = eps
+        self.max_iter = max_iter
+        self.reduction = reduction
+        self.focal_loss = focal_loss
+        self.focal_alpha = focal_alpha
+        self.cost_class = cost_class
+        self.cost_bbox = cost_bbox
+        self.cost_giou = cost_giou
+
+    def linear_sum_assignment(self, mu, nu, C):
+        u = torch.ones_like(mu)
+        v = torch.ones_like(nu)
+
+        # Sinkhorn iterations
+        for i in range(self.max_iter):
+            v = self.eps * \
+                (torch.log(
+                    nu + 1e-8) - torch.logsumexp(self.M(C, u, v).transpose(-2, -1), dim=-1)) + v
+            u = self.eps * \
+                (torch.log(
+                    mu + 1e-8) - torch.logsumexp(self.M(C, u, v), dim=-1)) + u
+
+        U, V = u, v
+        # Transport plan pi = diag(a)*K*diag(b)
+        pi = torch.exp(
+            self.M(C, U, V)).detach()
+
+        a = np.arange(np.min(C.shape))
+        b = torch.argmax(pi, dim=0)
+        indices = np.argsort(b)
+        return (b[indices], a[indices])
+
+        # Sinkhorn distance
+        # cost = torch.sum(
+        #     pi * C, dim=(-2, -1))
+        # return pi
+
+    def M(self, C, u, v):
+        '''
+        "Modified cost for logarithmic updates"
+        "$M_{ij} = (-c_{ij} + u_i + v_j) / epsilon$"
+        '''
+        return (-C + u.unsqueeze(-1) + v.unsqueeze(-2)) / self.eps
+
+
+    @torch.no_grad()
+    def forward(self, outputs, targets):
+        """ Performs the matching
+
+        Params:
+            outputs: This is a dict that contains at least these entries:
+                 "pred_logits": Tensor of dim [batch_size, num_queries, num_classes] with the
+                                classification logits
+                 "pred_boxes": Tensor of dim [batch_size, num_queries, 4] with the predicted
+                               box coordinates
+
+            targets: This is a list of targets (len(targets) = batch_size), where each target
+                     is a dict containing:
+                 "labels": Tensor of dim [num_target_boxes] (where num_target_boxes is the number
+                           of ground-truth objects in the target) containing the class labels
+                 "boxes": Tensor of dim [num_target_boxes, 4] containing the target box coordinates
+
+        Returns:
+            A list of size batch_size, containing tuples of (index_i, index_j) where:
+                - index_i is the indices of the selected predictions (in order)
+                - index_j is the indices of the corresponding selected targets (in order)
+            For each batch element, it holds:
+                len(index_i) = len(index_j) = min(num_queries, num_target_boxes)
+        """
+        batch_size, num_queries = outputs["pred_logits"].shape[:2]
+
+        # We flatten to compute the cost matrices in a batch
+        #
+        # [batch_size * num_queries, num_classes]
+        out_prob = outputs["pred_logits"].flatten(0, 1).softmax(-1)
+        # [batch_size * num_queries, 4]
+        out_bbox = outputs["pred_boxes"].flatten(0, 1)
+
+        # Also concat the target labels and boxes
+        tgt_ids = torch.cat([v["labels"] for v in targets])
+        tgt_bbox = torch.cat([v["boxes"] for v in targets])
+
+        # Compute the classification cost.
+        if self.focal_loss:
+            gamma = 2.0
+            neg_cost_class = (1 - self.focal_alpha) * (out_prob ** gamma) * (-(1 - out_prob + 1e-8).log())
+            pos_cost_class = self.focal_alpha * ((1 - out_prob) ** gamma) * (-(out_prob + 1e-8).log())
+            cost_class = pos_cost_class[:, tgt_ids] - neg_cost_class[:, tgt_ids]
+        else:
+            # Contrary to the loss, we don't use the NLL, but approximate it in 1 - proba[target class].
+            # The 1 is a constant that doesn't change the matching, it can be ommitted.
+            cost_class = -out_prob[:, tgt_ids]
+
+        # Compute the L1 cost between boxes
+        cost_bbox = torch.cdist(out_bbox, tgt_bbox, p=1)
+
+        # Compute the giou cost betwen boxes
+        cost_giou = -generalized_box_iou(
+            box_cxcywh_to_xyxy(out_bbox),
+            box_cxcywh_to_xyxy(tgt_bbox))
+
+        # Final cost matrix
+        cost_matrix = self.cost_bbox * cost_bbox \
+            + self.cost_class * cost_class \
+            + self.cost_giou * cost_giou
+        cost_matrix = cost_matrix.view(batch_size, num_queries, -1).cpu()
+
+        sizes = [len(v["boxes"]) for v in targets]
+
+        for i, target in enumerate(targets):
+            if 'track_query_match_ids' not in target:
+                continue
+
+            prop_i = 0
+            for j, mask_value in enumerate(target['track_queries_match_mask']):
+                if mask_value.item() == 1:
+                    track_query_id = target['track_query_match_ids'][prop_i]
+                    prop_i += 1
+
+                    cost_matrix[i, j] = np.inf
+                    cost_matrix[i, :, track_query_id + sum(sizes[:i])] = np.inf
+                    cost_matrix[i, j, track_query_id + sum(sizes[:i])] = -1
+                elif mask_value.item() == -1:
+                    cost_matrix[i, j] = np.inf
+
+        mu = torch.ones(cost_matrix.shape[1])
+        nu = torch.ones(cost_matrix.shape[2])
+
+        indices = [self.linear_sum_assignment(mu, nu, c[i])
+                   for i, c in enumerate(cost_matrix.split(sizes, -1))]
+
+        return [(torch.as_tensor(i, dtype=torch.int64), torch.as_tensor(j, dtype=torch.int64))
+                for i, j in indices]
+
+
 def build_matcher(args):
-    return HungarianMatcher(
-        cost_class=args.set_cost_class,
-        cost_bbox=args.set_cost_bbox,
-        cost_giou=args.set_cost_giou,
-        focal_loss=args.focal_loss,
-        focal_alpha=args.focal_alpha,)
+    if args.matcher == 'sinkhorn':
+        return SinkhornDistance(focal_loss=args.focal_loss,
+            focal_alpha=args.focal_alpha)
+    elif args.matcher == 'Hungarian':
+        return HungarianMatcher(
+            cost_class=args.set_cost_class,
+            cost_bbox=args.set_cost_bbox,
+            cost_giou=args.set_cost_giou,
+            focal_loss=args.focal_loss,
+            focal_alpha=args.focal_alpha,)
